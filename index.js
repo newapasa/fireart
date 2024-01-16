bc8f1a7db01c0a9d10b0e7943bde2ebb460b1ae4
Shreejan_Prajapati <newapasa7@gmail.com>
2024-01-16 17:25:04 +0545

    Fix js file

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-    //footer click start
-    $(".fotterlist1 h3").click(function(){
-        $(".fotterlist1 h3").removeClass("active");
-        $(this).addClass("active");
-        $(".click").removeClass("active");
-        $("#" + $(this).attr("data-click")).addClass("active");
-        });
-    //footer click end
+function toggleHide(elementId) {
+  const element = document.getElementById(elementId);
 
-    function toggleHide(elementId){
-        const element = document.getElementById(elementId);
-    
-        element.classList.toggle("hide-lists");
-    }
-    
-    function toggleHideItems(elementId){
-        const element = document.getElementById(elementId);
-    
-        element.classList.toggle("show-list-items");
-        element.classList.toggle("hide-list-items");
-    }
+  element.classList.toggle("hide-lists");
+}
+
+function toggleHideItems(elementId) {
+  const element = document.getElementById(elementId);
+
+  element.classList.toggle("show-list-items");
+  element.classList.toggle("hide-list-items");
+}
